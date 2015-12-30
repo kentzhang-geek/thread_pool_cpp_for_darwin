1cc627a070fcd6496b5894c0ce69a4c938db89d6
Kent_Zhang <642238553@qq.com>
2015-12-30 16:45:55 +0800

    ver.0.1 finish and publiced

---
+++ README.md
@@ -0,0 +1,10 @@
+CPP thread pool for mac
+===========================
+
+Only 2 source file import to your project then use thread pool easy and simple:
+libthreadpool.hpp
+libthreadpool.cpp
+
+All documents in libthreadpool.hpp
+
+Kent Zhang
--- threadpool.hpp
+++ threadpool.hpp
@@ -1,5 +1,7 @@
 /**
  * Do What You Want To Do Lisence
+ *
+ * Description : Actors in arenas. Seems like thread instances in thread pool.
  */
 
 #ifndef __CPP_THREAD_POOL_TRY_TO_WRITE__
@@ -14,6 +16,12 @@
 #include <semaphore.h>
 #include <stdio.h>
 
+/**
+ * inherit this class and overwrite run_once or run_loop to run your code in thread
+ * 
+ * Notice that run_loop will only be called once, your should realize your own loop
+ * in run_loop if needed
+ */
 class thread_handle {
 public:
 	void * thread_ctx;
@@ -24,6 +32,18 @@ public:
 	virtual int run_loop() {return 0;};
 };
 
+/**
+ * this class realize a thread pool, all thread begin to run once your created pool, 
+ * threads will wait for actors require arena
+ * Once an actor is ready for play his role, pool will add actor to queue and post a
+ * semphore to acknowledge arena. Then arena will get semphore and get an actor from
+ * queue to run.
+ *
+ * Notice : 
+ * @param actor_name in method add_actor is not required yet, it will be
+ * ignored
+ * method remove_actor is empty yet, it will return imediatelly. 
+ */
 class thread_pool {
 protected:
 	std::map<char *, thread_handle *> * actors;
